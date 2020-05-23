197cfed48b0f954d35b2afcaa9edfe51a9b20dfb
Brenden Smerbeck <bsmerbeck@gmail.com>
2020-05-23 15:09:05 -0400

    Added firebase

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-
-import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router'; // react-router v4/v5
-import { ConnectedRouter } from 'connected-react-router';
-import configureStore, { history } from './state/configureStore';
-
+import { Provider } from "react-redux";
+import firebase from "firebase";
+import { firebaseConfig } from "./firebase";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { createFirestoreInstance } from "redux-firestore";
+import { Route, Switch } from "react-router"; // react-router v4/v5
+import { ConnectedRouter } from "connected-react-router";
+import configureStore, { history } from "./state/configureStore";
 
 const store = configureStore();
 
+const rrfProps = {
+  firebase,
+  config: firebaseConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
+
 ReactDOM.render(
   <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
       <ConnectedRouter history={history}>
-          <Switch>
-              <Route exact path="/" render={() => <App/>}/>
-          </Switch>
+        <Switch>
+          <Route exact path="/" render={() => <App />} />
+        </Switch>
       </ConnectedRouter>
+    </ReactReduxFirebaseProvider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/state/ducks/index.js
+++ src/state/ducks/index.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
+import { firebaseReducer, firestoreReducer } from "react-redux-firebase";
 
-
-const rootReducer = history =>
-    combineReducers( {
-        router: connectRouter(history)
-    });
+const rootReducer = (history) =>
+  combineReducers({
+    router: connectRouter(history),
+    firebase: firebaseReducer,
+    firestore: firestoreReducer,
+  });
 
 export default rootReducer;
