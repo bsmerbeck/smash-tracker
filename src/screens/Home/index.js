d6c829e2eef4301148c3d0157c89deb4d3dc1015
Brenden Smerbeck <bsmerbeck@gmail.com>
2020-05-23 14:25:13 -0400

    Added dependencies for router, redux

---
+++ src/screens/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HomeContainer from './HomeContainer';
+
+
+const HomeScreen = ({className }) => (
+    <HomeContainer className={className}/>
+);
+
+export default HomeScreen;
