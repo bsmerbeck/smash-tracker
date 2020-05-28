21f35ecaa95f298dc8a498f7959745bf30f2e983
Brenden Smerbeck <bsmerbeck@gmail.com>
2020-05-28 17:24:49 -0400

    fixed functions for user profile generation on account creation

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+admin.initializeApp();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+exports.createProfile = functions.auth
+  .user()
+  .onCreate((userRecord, context) => {
+    return admin.database().ref(`/users/${userRecord.uid}`).set({
+      email: userRecord.email,
+    });
+  });
