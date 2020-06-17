c24c7f691f09c771213a046bfdfc16c3de6a69d2
Brenden Smerbeck <bsmerbeck@gmail.com>
2020-06-17 15:57:40 -0400

    quick feature: refine matchups list to only characters you've recorded matches with. Next will also filter on fighter.

--- src/screens/Matchups/components/SelectOpponent/SelectOpponent.js
+++ src/screens/Matchups/components/SelectOpponent/SelectOpponent.js
@@ -7,18 +7,36 @@ import ListItemText from "@material-ui/core/ListItemText";
 import { StyledIconSelect } from "./style";
 import { SpriteList } from "../../../../components/Sprites/SpriteList";
 
+const operation = (list1, list2, isUnion = false) =>
+  list1.filter(
+    ((set) => (a) => isUnion === set.has(a.id))(new Set(list2.map((b) => b.id)))
+  );
+
+const inBoth = (list1, list2) => operation(list1, list2, true);
+
 const SelectOpponent = () => {
-  const context = useContext(MatchupsContext);
+  const { matches, auth, updateOpponent } = useContext(MatchupsContext);
+
+  const entries = Object.keys(matches[auth.uid]);
+  const real_matches = entries.map((e) => matches[auth.uid][e]);
+
+  const opponents = real_matches.map((o) => {
+    return {
+      id: o.opponent_id,
+    };
+  });
+
+  const spriteBoth = inBoth(SpriteList, opponents);
 
   const handleSelect = (e) => {
-    context.updateOpponent(e.target.value);
+    updateOpponent(e.target.value);
   };
   return (
     <MatchupsContext.Consumer>
       {({ opponent }) => (
         <FormControl>
           <StyledIconSelect value={opponent} onChange={handleSelect}>
-            {SpriteList.map((s) => {
+            {spriteBoth.map((s) => {
               return (
                 <MenuItem value={s} key={s.id}>
                   <ListItemIcon>
