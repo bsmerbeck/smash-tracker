38160b7394eb3e90557f5215fd1d09893d0638c2
Brenden Smerbeck <bsmerbeck@gmail.com>
2020-08-11 17:27:30 -0400

    fixed refactoring issue with SelectFighter on dashboard

--- src/screens/Dashboard/components/index.js
+++ src/screens/Dashboard/components/index.js
@@ -1,5 +1,5 @@
 export { default as DashboardToolbar } from "./DashboardToolbar";
-export { default as SelectFighter } from "../../FighterAnalysis/components/SelectFighter";
+export { default as SelectFighter } from "./SelectFighter";
 export { default as LastMatchesChart } from "./LastMatchesChart";
 export { default as WinLossTracker } from "./WinLossTracker";
 export { default as BestWorstMatchup } from "./BestWorstMatchup";
