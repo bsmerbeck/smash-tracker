ba2583d07b907680c3e2976ae7f2de508cc83c3b
Brenden Smerbeck <bsmerbeck@gmail.com>
2020-08-11 17:27:30 -0400

    fixed refactoring issue with SelectFighter on dashboard

--- src/screens/Dashboard/components/SelectFighter/SelectFighter.js
+++ src/screens/Dashboard/components/SelectFighter/SelectFighter.js
@@ -1,22 +1,20 @@
 import React from "react";
-import { FighterAnalysisContext } from "../../FighterAnalysis";
+import { DashboardContext } from "../../Dashboard";
 import MenuItem from "@material-ui/core/MenuItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import { StyledIconSelect } from "./style";
 
-const SelectFighter = () => {
-  const context = React.useContext(FighterAnalysisContext);
-
-  const { fighter, fighterSprites, updateSprite } = context;
+const SelectFighter = (props) => {
+  const context = React.useContext(DashboardContext);
 
   const handleSelect = (e) => {
-    updateSprite(e.target.value);
+    context.updateSprite(e.target.value);
   };
 
   return (
-    <StyledIconSelect value={fighter} onChange={(e) => handleSelect(e)}>
-      {fighterSprites.map((s) => {
+    <StyledIconSelect value={context.fighter} onChange={(e) => handleSelect(e)}>
+      {context.fighterSprites.map((s) => {
         return (
           <MenuItem value={s} key={s.id}>
             <ListItemIcon>
---
+++ src/screens/Dashboard/components/SelectFighter/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+import Select from "@material-ui/core/Select";
+
+export const StyledIconSelect = styled(Select)`
+  .MuiInputBase-input {
+    display: flex;
+    align-items: center;
+  }
+`;
